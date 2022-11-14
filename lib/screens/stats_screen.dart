9f04dbf8e59f31faff5492d7aea982967429b543
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-11-15 03:34:49 +0530

    stats all done

--- lib/screens/stats_screen.dart
+++ lib/screens/stats_screen.dart
@@ -2,9 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/widgets/stats/food_category_chart.dart';
 import 'package:flutter_application_1/widgets/stats/meals_chart.dart';
 import 'package:flutter_application_1/widgets/stats/most_prefered_foods.dart';
+import '../services/stats/stats_services.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class StatsScreen extends StatelessWidget {
+class StatsScreen extends StatefulWidget {
   const StatsScreen({super.key});
+
+  @override
+  State<StatsScreen> createState() => _StatsScreenState();
+}
+
+class _StatsScreenState extends State<StatsScreen> {
+  bool loading = true;
+  bool active = false;
+  String message = "";
+
+  void getData() async {
+    var response = await haveActiveDietPlan();
+    if (response is String) {
+      setState(() {
+        message = response;
+      });
+    }
+    if (response is bool) {
+      if (response) {
+        setState(() {
+          active = response;
+        });
+      } else {
+        setState(() {
+          message = "No Active Diet Plan yet ..!";
+        });
+      }
+    }
+
+    setState(() {
+      loading = false;
+    });
+    print(response);
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,22 +55,41 @@ class StatsScreen extends StatelessWidget {
         title: const Text("More about your Diet Plan"),
         backgroundColor: Colors.green[500],
       ),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.white,
-        child: ListView(children: [
-          MealsChart(),
-          const SizedBox(
-            height: 20,
-          ),
-          FoodCategoryChart(),
-          const SizedBox(
-            height: 20,
-          ),
-          MostOccuringFoods(),
-        ]),
-      ),
+      body: active
+          ? Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              color: Colors.white,
+              child: ListView(children: [
+                MealsChart(),
+                const SizedBox(
+                  height: 20,
+                ),
+                FoodCategoryChart(),
+                const SizedBox(
+                  height: 20,
+                ),
+                MostOccuringFoods(),
+              ]),
+            )
+          : loading
+              ? const Center(
+                  child: SpinKitSpinningLines(
+                    color: Colors.blueGrey,
+                    size: 50.0,
+                  ),
+                )
+              : Center(
+                  child: Text(
+                  message,
+                  style: const TextStyle(
+                    color: Colors.blueGrey,
+                    fontSize: 15,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: 2,
+                    wordSpacing: 10,
+                  ),
+                )),
     );
   }
 }
--- lib/services/stats/stats_services.dart
+++ lib/services/stats/stats_services.dart
@@ -59,3 +59,21 @@ Future<Object> getFoodCategoryPercentage() async {
     return 'Something went Wrong';
   }
 }
+
+Future<Object> haveActiveDietPlan() async {
+  
+  var response = await api_service
+      .fetchPost("http://192.168.8.101:4000/user/activeplan", {
+    "user_Id": "6360cf9f0ebc552ba5863f87",
+  });
+
+  if (response.statusCode == 200) {
+    var d = json.decode(response.body)['active'];
+    print(d);
+    return d;
+  } else {
+    print(response);
+    return 'Something went Wrong';
+  }
+}
+
