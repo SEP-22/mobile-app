59e91e518ae1b54e31246c18c5fd8d1276624fa4
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-11-16 23:20:30 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Eat Smart',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch:Colors.green,
       ),
       home: const HomeScreen(),
       routes: {
